155168b880ea8c72df4f8d0546f4dbaffdd78a5f
yutakahashi <yuu.takahashi@idealump.com>
2024-10-08 15:03:55 +0900

    “課題3の実装”

--- src/PostsDetails.jsx
+++ src/PostsDetails.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import posts from './data/posts.js';
-
 import styles from './cssModule/blogList.module.css'
 
 const PostsDetails = () => {
   const { postId } = useParams();
-  const post = posts.find((elem) => elem.id.toString() === postId);
+  const [post, setPost] = useState();
+  const [loading, setLoading] = useState(true);
+
+  // APIでpostsを取得する処理をuseEffectで実行します。
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+        const res = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${postId}`);
+        const data = await res.json();
+        setPost(data.post);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetcher();
+  }, [postId]);
+
+  if(loading) {
+    return <p>Loading...</p>;
+  }
+
   const date = new Date(post.createdAt);
   const dateText = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
 
@@ -16,15 +35,15 @@ const PostsDetails = () => {
   return(
     <article className={styles.post}>
       <div className={styles.post__img}>
-        <img src={post.thumbnailUrl} />
+        <img src={post.thumbnailUrl} alt={post.title} />
       </div>
       <div className={styles.post__content}>
         <div className={styles.post__head}>
           <p className={styles.post__date}>{dateText}</p>
           <ul className={styles.category}>
-            {post.categories.map(category =>
+            {post.categories.map(category => (
               <li className={styles.category__item} key={category}>{category}</li>
-            )}
+              ))}
           </ul>
         </div>
         <h1 className={styles.post__title}>{post.title}</h1>
